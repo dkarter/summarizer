4a2edfcdb5383554e9e48733a86c27be1d37e979
Dorian Karter <dkarter@users.noreply.github.com>
2024-10-20 00:28:48 -0500

    chore: remove styler - too annoying

--- mix.exs
+++ mix.exs
@@ -21,8 +21,6 @@ defmodule Summerizer.MixProject do
 
   # Run "mix help deps" to learn about dependencies.
   defp deps do
-    [
-      {:styler, "~> 1.1", only: [:dev, :test], runtime: false}
-    ]
+    []
   end
 end
