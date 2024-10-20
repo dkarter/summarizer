fcf2b3fa44283acf03911c7b250eae2a67435d9f
Dorian Karter <dkarter@users.noreply.github.com>
2024-10-20 00:29:44 -0500

    chore: enable observer

--- mix.exs
+++ mix.exs
@@ -14,7 +14,7 @@ defmodule Summerizer.MixProject do
   # Run "mix help compile.app" to learn about applications.
   def application do
     [
-      extra_applications: [:logger],
+      extra_applications: [:logger, :wx, :observer, :runtime_tools],
       mod: {Summerizer.Application, []}
     ]
   end
